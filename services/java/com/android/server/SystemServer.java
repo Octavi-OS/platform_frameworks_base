55cf4f889dc8875918c0bbdb7b4eaebca61e112a
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-06-15 15:48:07 -0300

    Initial import of lineagehw
    All credits to LineageOS and related authors on:
    
    https://github.com/LineageOS/android_lineage-sdk
    
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Change-Id: Ief90ce8c20115d6a1c17c84a9874af6246dc1dcd

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -201,6 +201,9 @@ import java.util.Timer;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Future;
 
+// LineageHardware
+import com.android.server.custom.LineageHardwareService;
+
 public final class SystemServer {
 
     private static final String TAG = "SystemServer";
@@ -2088,6 +2091,13 @@ public final class SystemServer {
             t.traceBegin("StartPocketService");
             mSystemServiceManager.startService(PocketService.class);
             t.traceEnd();
+
+            // LineageHardware
+            if (!mOnlyCore){
+                t.traceBegin("StartLineageHardwareService");
+                mSystemServiceManager.startService(LineageHardwareService.class);
+                t.traceEnd();
+            }
 
             if (!context.getResources().getString(
                     com.android.internal.R.string.config_pocketBridgeSysfsInpocket).isEmpty()) {
