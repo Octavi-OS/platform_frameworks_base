c3e05f3096421abbb0e16d30ee1c43b8cde9f0bb
ezio84 <ezio84@users.noreply.github.com>
2020-09-29 15:50:36 +0000

    [1/2] base: Double tap to sleep on lockscreen
    arrow-11.0 edits:
    - Adapt code to refactored status bar controllers
    
    Change-Id: Icd8de7c5512362f22dcea9c3946347a1f0c13aba

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4884,6 +4884,12 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
         /**
+         * Double tap on lockscreen to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_LOCKSCREEN = "double_tap_sleep_lockscreen";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
@@ -5023,6 +5029,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(ANBI_ENABLED_OPTION);
             PRIVATE_SETTINGS.add(ALLOW_INCALL_HOME);
             PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_GESTURE);
+            PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_LOCKSCREEN);
         }
 
         /**
