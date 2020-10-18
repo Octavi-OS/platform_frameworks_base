9b8c80d04c6652cced06a93717b0c963c0ef3514
Sagarrokade006 <Sagarrokade006@gmail.com>
2020-10-18 20:12:31 +0000

    [SQUASH] Revert Auto Brightness Toggle Commits
    Revert "Brightness slider: Visual Improvements"
    
    This reverts commit 5c900f0d4d54c1b83855ce64b1489149a71acf70.
    
    Revert "SystemUI: use OnePlus drawables for autobrightness icon"
    
    This reverts commit aed6d30c7043328b8954686ddbc03b9463b61521.
    
    Revert "Add auto brightness toggle to Quick Settings"
    
    This reverts commit b2a3b2a0e5b516e783953ce3b9f1bd3805693282.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1261,7 +1261,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                             .withDefault(this::createDefaultQSFragment)
                             .build());
             mBrightnessMirrorController = new BrightnessMirrorController(
-                    mContext,
                     mNotificationShadeWindowView,
                     mNotificationPanelViewController,
                     mNotificationShadeDepthControllerLazy.get(),
