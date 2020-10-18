a2dcf10f600da85b2b13c565466d8c5e26891834
Sagarrokade006 <Sagarrokade006@gmail.com>
2020-10-18 19:40:39 +0000

    [SQUASH] Revert Network Traffic Indicator
    Revert "NetworkTraffic: Update for Android R"
    
    This reverts commit 11615ddab7212e785c140d47749f71aa344defd2.
    
    Revert "NetworkTraffic: Hide arrows in statusbar [1/2]"
    
    This reverts commit 7d34817dbae2be81866af1d43b5c2606750f5ae4.
    
    Revert "NetworkTraffic: Make the indicator arrows dynamic"
    
    This reverts commit 20b530fa6159a534205cd9d72667d4917a273503.
    
    Revert "NetworkTraffic: Switch to OOS style indicator"
    
    This reverts commit c610505bcb74de52c7a1eba6374ea63c9f9361b9.
    
    Revert "NetworkTraffic: Fix indicator not hiding on lost connection"
    
    This reverts commit e51d6059e176a84f6a8379b31b872943a6939750.
    
    Revert "NetworkTraffic: Fix static color usage"
    
    This reverts commit 9924139d9ba6264fc0205e2e21ae18a072247b15.
    
    Revert "NetworkTraffic: Add it properly to statusbar icons"
    
    This reverts commit d12662a5793e68a0d8f69f3cae7dcccf2922cbf1.
    
    Revert "NetworkTraffic: Skip more useless handler calls"
    
    This reverts commit 02aff8132e95ac80739a16751fd0063c6c4f4af0.
    
    Revert "NetworkTraffic: Stop the handler if screen is off"
    
    This reverts commit 49cdb877cf75c28b991c4eeed6c7e065e3f708ad.
    
    Revert "NetworkTraffic: Fix janky traffic indicator tint"
    
    This reverts commit 95ecf747685aa99e9657b78f7986db5b5d184288.
    
    Revert "base: Statusbar network traffic [1/2]"
    
    This reverts commit 042a08c88e58a8bbedc96701674d26b686b4a20b.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5160,24 +5160,6 @@ public final class Settings {
        public static final String BATTERY_LIGHT_REALLYFULL_COLOR = "battery_light_reallyfull_color";
 
         /**
-         * Wheter to show network traffic indicator in statusbar
-         * @hide
-         */
-        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
-
-        /**
-         * Network traffic inactivity threshold (default is 1 kBs)
-         * @hide
-         */
-        public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
-
-        /**
-         * Whether to disable showing arrows in statusbar network traffic indicators
-         * @hide
-         */
-        public static final String NETWORK_TRAFFIC_ARROW = "network_traffic_arrow";
-
-        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
