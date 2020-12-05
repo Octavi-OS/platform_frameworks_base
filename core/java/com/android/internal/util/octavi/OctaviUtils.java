785154ff40d2d182ecd65bc7b91e1d3807669ec4
BoredOutOfMyGit <BoredOutOfMyGit@users.noreply.github.com>
2020-12-05 07:34:33 +0000

    ActionUtils: Add screen on action
    Add an action to force the screen to turn on. This is a quick
    and effective way to turn on the screen when we need it to. Intead
    of hijacking WindowManagerPolicy in PWM, we instead call on PM to
    do this.
    
    - Make sure 'screen off' action doesn't work while screen is off.
    This action should not be called if the screen is already off.
    
    Change-Id: Icacb721ebc6493e4bd0db9d02ac10d66f22eb143
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Signed-off-by: Sagarrokade006 <sagarrokade006@gmail.com>

--- core/java/com/android/internal/util/octavi/OctaviUtils.java
+++ core/java/com/android/internal/util/octavi/OctaviUtils.java
@@ -196,9 +196,24 @@ public class OctaviUtils {
                 networkOperator.substring(0, /*Filter only 3 digits*/ 3));
     }
 
+    // Screen on
+    public static void switchScreenOn(Context context) {
+        PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        if (pm == null) return;
+        PowerManager.WakeLock wakeLock = pm.newWakeLock((
+                PowerManager.SCREEN_BRIGHT_WAKE_LOCK |
+                        PowerManager.ACQUIRE_CAUSES_WAKEUP), "wakeup:device");
+        boolean isScreenOn = pm.isScreenOn();
+        /* Wake up the device only when screen is off.
+         * Otherwise don't bother to do anything. */
+        if (!wakeLock.isHeld() && !isScreenOn) {
+            wakeLock.acquire();
+        }
+    }
+
     public static void switchScreenOff(Context ctx) {
         PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
-        if (pm!= null) {
+        if (pm!= null && pm.isScreenOn()) {
             pm.goToSleep(SystemClock.uptimeMillis());
         }
     }
