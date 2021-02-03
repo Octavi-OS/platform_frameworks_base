db921086d611ed55effda06be0c6079786bc9360
ezio84 <ezio84@users.noreply.github.com>
2021-02-03 18:09:27 +0000

    base: Fix custom lockscreen shortcut icons being white
    now 8.1 sets a tint to make icons black and white according to the
    wallpaper main color but this is not suitable for custom icons
    let's fix this setting the tint only for default lockscreen buttons
    
    Change-Id: I79c39fb39e308f9815afda0c85fcd3922c528c69
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>
    Signed-off-by: Sagarrokade006 <Sagarrokade006@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/LockIcon.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/LockIcon.java
@@ -76,7 +76,7 @@ public class LockIcon extends KeyguardAffordanceView {
                         R.string.accessibility_scanning_face));
             }
 
-            setImageDrawable(icon, false);
+            setImageDrawable(icon, false, false);
             shakeFace();
 
             if (icon instanceof AnimatedVectorDrawable) {
--- packages/SystemUI/src/com/android/systemui/tuner/LockscreenFragment.java
+++ packages/SystemUI/src/com/android/systemui/tuner/LockscreenFragment.java
@@ -327,6 +327,8 @@ public class LockscreenFragment extends PreferenceFragment {
         @Override
         public IntentButton create(Map<String, String> settings) {
             String buttonStr = settings.get(mKey);
+            // if buttonStr is not empty it means it's not the Default button,
+            // so we'll create a custom one
             if (!TextUtils.isEmpty(buttonStr)) {
                 if (buttonStr.contains("::")) {
                     return new ShortcutButton(mContext, buttonStr);
@@ -359,6 +361,7 @@ public class LockscreenFragment extends PreferenceFragment {
             mIconState.drawable = new ScalingDrawableWrapper(mIconState.drawable,
                     mSize / (float) mIconState.drawable.getIntrinsicWidth());
             mIconState.tint = false;
+            mIconState.isDefaultButton = false;
             init();
         }
 
@@ -409,6 +412,7 @@ public class LockscreenFragment extends PreferenceFragment {
             mIconState.drawable = new ScalingDrawableWrapper(mIconState.drawable,
                     mSize / (float) mIconState.drawable.getIntrinsicWidth());
             mIconState.tint = false;
+            mIconState.isDefaultButton = false;
             init();
         }
 
@@ -448,6 +452,7 @@ public class LockscreenFragment extends PreferenceFragment {
         public HiddenButton() {
             mIconState = new IconState();
             mIconState.isVisible = false;
+            mIconState.isDefaultButton = false;
         }
 
         @Override
