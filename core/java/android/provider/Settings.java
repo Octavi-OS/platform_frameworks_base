72a6ed8573219a43adca507f4e3ff1e1a00cfa10
anayw2001 <anayw2001@users.noreply.github.com>
2021-02-04 17:17:02 +0000

    base: clipboard toast toggle [1/2]
    Change-Id: I1c8c47b1865eae864e44f4e0b682dee4fe07c827
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>
    Signed-off-by: Sagarrokade006 <Sagarrokade006@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5865,6 +5865,12 @@ public final class Settings {
         public static final String USE_OLD_MOBILETYPE = "use_old_mobiletype";
 
         /**
+         * Toggle for clipboard access toast.
+         * @hide
+         */
+        public static final String SHOW_CLIPBOARD_TOAST = "show_clipboard_toast";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
