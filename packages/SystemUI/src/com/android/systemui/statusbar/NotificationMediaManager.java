Octavi-OS/platform_frameworks_base

commit b20c0567f16271c7baf74e70f1bcc9c301f3d1d3

Lockscreen MediaArt: hide if media is not playing
it's annoying to get the artwork even if the notification gets
swiped out and media is paused, because the media session is still alive

Change-Id: I3540e1429524d34029d273c5cbe216fa46e4d59e

packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -638,7 +638,9 @@ public class NotificationMediaManager implements Dumpable {
             }
         }
 
-        if ((hasArtwork || DEBUG_MEDIA_FAKE_ARTWORK)
+        // show artwork only if the media is playing
+        if (PlaybackState.STATE_PLAYING == getMediaControllerPlaybackState(mMediaController)
+                && (hasArtwork || DEBUG_MEDIA_FAKE_ARTWORK)
                 && (mStatusBarStateController.getState() != StatusBarState.SHADE || allowWhenShade)
                 &&  mBiometricUnlockController != null && mBiometricUnlockController.getMode()
                         != BiometricUnlockController.MODE_WAKE_AND_UNLOCK_PULSING

commit a2837db3876d755403aef23d517995cb1d4951af

Fix lockscreen wallpaper not being set if different from the Home one
this got broken with b20c0567f16271c7baf74e70f1bcc9c301f3d1d3

thanks to @anayw2001 who digged into commits chain and found
the root of the issue

Change-Id: Ifc96f6e540897a43fdd3cf23c8ac518a7f2f77e2

packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -627,11 +627,14 @@ public class NotificationMediaManager implements Dumpable {
     private void finishUpdateMediaMetaData(boolean metaDataChanged, boolean allowEnterAnimation,
             @Nullable Bitmap bmp) {
         Drawable artworkDrawable = null;
-        if (bmp != null) {
+        // set media artwork as lockscreen wallpaper if player is playing
+        if (bmp != null && (mLockscreenArt || !ENABLE_LOCKSCREEN_WALLPAPER) &&
+                PlaybackState.STATE_PLAYING == getMediaControllerPlaybackState(mMediaController)) {
             artworkDrawable = new BitmapDrawable(mBackdropBack.getResources(), bmp);
         }
         boolean hasMediaArtwork = artworkDrawable != null;
         boolean allowWhenShade = false;
+        // if no media artwork, show normal lockscreen wallpaper
         if (ENABLE_LOCKSCREEN_WALLPAPER && artworkDrawable == null) {
             Bitmap lockWallpaper =
                     mLockscreenWallpaper != null ? mLockscreenWallpaper.getBitmap() : null;
@@ -670,9 +673,7 @@ public class NotificationMediaManager implements Dumpable {
             }
         }
 
-        // show artwork only if the media is playing
-        if (PlaybackState.STATE_PLAYING == getMediaControllerPlaybackState(mMediaController)
-                && (hasArtwork || DEBUG_MEDIA_FAKE_ARTWORK)
+        if ((hasArtwork || DEBUG_MEDIA_FAKE_ARTWORK)
                 && (mStatusBarStateController.getState() != StatusBarState.SHADE || allowWhenShade)
                 &&  mBiometricUnlockController != null && mBiometricUnlockController.getMode()
                         != BiometricUnlockController.MODE_WAKE_AND_UNLOCK_PULSING