7149f398f7dee63515268e10dc57875453d5cacd
LorDClockaN <LorDClockaN@users.noreply.github.com>
2021-03-02 18:14:40 +0000

    base: Sensor block per-package switch (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5964,6 +5964,12 @@ public final class Settings {
         public static final String SHOW_FPS_OVERLAY = "show_fps_overlay";
 
         /**
+         * Sensor block per-package
+         * @hide
+         */
+        public static final String SENSOR_BLOCK = "sensor_block";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
