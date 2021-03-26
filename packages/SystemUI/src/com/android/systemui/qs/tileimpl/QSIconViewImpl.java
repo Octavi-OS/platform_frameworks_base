d11808f44b38f546fb25dfeb7dd6bf3ecf708cba
Rahul <rahul731999@gmail.com>
2021-03-26 21:00:13 +0530

    Revert "base: SystemUI: Tint QS Tile with accent color"
    This reverts commit 4826bda8035abc0788a2afbfcc91578f291f9514.

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSIconViewImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSIconViewImpl.java
@@ -197,11 +197,7 @@ public class QSIconViewImpl extends QSIconView {
                 int alpha = (int) (fromAlpha + (toAlpha - fromAlpha) * fraction);
                 int channel = (int) (fromChannel + (toChannel - fromChannel) * fraction);
 
-                if (setQsUseNewTint) {
-                    setTint(iv, toColor);
-                } else {
-                    setTint(iv, Color.argb(alpha, channel, channel, channel));
-                }
+                setTint(iv, Color.argb(alpha, channel, channel, channel));
             });
             anim.addListener(new AnimatorListenerAdapter() {
                 @Override
