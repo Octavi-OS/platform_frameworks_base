6f70f1acfe8140c17df0a722b798654939c19df4
Rahul <rahul731999@gmail.com>
2021-04-16 17:01:55 +0530

    Revert "SystemUI: Redesign Media Player View"
    This reverts commit a8918f789712c2687ca639b314235a2854140864.

--- packages/SystemUI/src/com/android/systemui/media/MediaControlPanel.java
+++ packages/SystemUI/src/com/android/systemui/media/MediaControlPanel.java
@@ -263,6 +263,10 @@ public class MediaControlPanel {
         TextView titleText = mViewHolder.getTitleText();
         titleText.setText(data.getSong());
 
+        // App title
+        TextView appName = mViewHolder.getAppName();
+        appName.setText(data.getApp());
+
         // Artist name
         TextView artistText = mViewHolder.getArtistText();
         artistText.setText(data.getArtist());
