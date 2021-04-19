6ed2072b8db8ec55aebe2a1bd36c18ca42ad77cd
TikkiTikki <radstylemusic@gmail.com>
2021-04-19 03:46:42 -0300

    Add support to see if there are visible notifications in ClockPlugin
    to create more lively and intelligent clocks as in Android S
    by @TikkiX2

--- packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitch.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitch.java
@@ -289,6 +289,7 @@ public class KeyguardClockSwitch extends RelativeLayout {
             mClockPlugin.setTextColor(getCurrentTextColor());
         }
         mClockPlugin.setDarkAmount(mDarkAmount);
+        mClockPlugin.setHasVisibleNotifications(mHasVisibleNotifications);
         if (mColorPalette != null) {
             mClockPlugin.setColorPalette(mSupportsDarkText, mColorPalette);
         }
@@ -378,6 +379,9 @@ public class KeyguardClockSwitch extends RelativeLayout {
             return;
         }
         mHasVisibleNotifications = hasVisibleNotifications;
+        if (mClockPlugin != null) {
+            mClockPlugin.setHasVisibleNotifications(mHasVisibleNotifications);
+        }
         if (mDarkAmount == 0f && mBigClockContainer != null) {
             // Starting a fade transition since the visibility of the big clock will change.
             TransitionManager.beginDelayedTransition(mBigClockContainer,
