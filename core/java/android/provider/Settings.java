1b643e23118eef77e6a7d3945d14469f605d635b
calebcabob <calebcabob@users.noreply.github.com>
2021-06-14 21:15:34 +0000

    Allow enabling/disabling Android 11 powermenu by ezio84 [1/2] Something similar to this can be done for customizing the power menu as well (add/remove items).
    Signed-off-by: calebcabob <calphonic@gmail.com>
    Change-Id: I9b58abcef9d88fbec6592fa31760c5d9d6ebeebc

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5101,6 +5101,12 @@ public final class Settings {
         public static final String VOLUME_PANEL_ON_LEFT = "volume_panel_on_left";
 
         /**
+         * Whether to show Advanced action in powermenu or not
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
