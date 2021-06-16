f545b4de1544d089362aebdc1659b4a99808a894
shagbag913 <sh4gbag913@gmail.com>
2021-06-16 20:54:55 +0000

    Permissions: Grant Google Sound Picker storage perms Change-Id: I18c5739e73b4734d6dd66ced7fe5e42c5d372109 Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -937,6 +937,9 @@ public final class DefaultPermissionGrantPolicy {
         // Google Chrome
         grantSystemFixedPermissionsToSystemPackage(pm, "com.android.chrome", userId, CONTACTS_PERMISSIONS,
                 STORAGE_PERMISSIONS);
+
+        // Google sound picker
+        grantSystemFixedPermissionsToSystemPackage(pm,"com.google.android.soundpicker", userId, STORAGE_PERMISSIONS);
     }
 
     private String getDefaultSystemHandlerActivityPackageForCategory(PackageManagerWrapper pm,
