f66aa620d5378dfe8a91b3a687b15a67f4c87908
neobuddy89 <neobuddy89@users.noreply.github.com>
2021-06-17 17:21:05 +0000

    base: Add bool to enable/disable doze by default

--- core/java/android/hardware/display/AmbientDisplayConfiguration.java
+++ core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -38,6 +38,7 @@ public class AmbientDisplayConfiguration {
     private final Context mContext;
     private final boolean mAlwaysOnByDefault;
     private final boolean mDeviceHasSoli;
+    private final boolean mDozeEnabledByDefault;
 
     /** {@hide} */
     @TestApi
@@ -45,6 +46,7 @@ public class AmbientDisplayConfiguration {
         mContext = context;
         mAlwaysOnByDefault = mContext.getResources().getBoolean(R.bool.config_dozeAlwaysOnEnabled);
         mDeviceHasSoli = mContext.getResources().getBoolean(R.bool.config_has_Soli);
+        mDozeEnabledByDefault = mContext.getResources().getBoolean(R.bool.config_doze_enabled_by_default);
     }
 
     /** {@hide} */
@@ -66,7 +68,7 @@ public class AmbientDisplayConfiguration {
 
     /** {@hide} */
     public boolean pulseOnNotificationEnabled(int user) {
-        return boolSettingDefaultOn(Settings.Secure.DOZE_ENABLED, user)
+        return boolSetting(Settings.Secure.DOZE_ENABLED, user, mDozeEnabledByDefault ? 1 : 0)
                 && pulseOnNotificationAvailable();
     }
 
