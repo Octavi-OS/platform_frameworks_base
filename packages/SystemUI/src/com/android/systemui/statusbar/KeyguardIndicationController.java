neobuddy89/frameworks_base

commit 34e1765ecdc5896e7340cf2a3af825864d3dc085

SystemUI: Fix showing duplicated charging status
In commit 85e5a57 it was possible that plugging a device in,
then turning on the screen would show the charging status twice.
To fix, don't append the charging status to if there is a
transient notification.

Change-Id: Ic031fbaf9a99d044f50dc65e2b189773222ebe80

packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -429,14 +429,7 @@ public class KeyguardIndicationController implements StateListener,
             if (!mKeyguardUpdateMonitor.isUserUnlocked(userId)) {
                 mTextView.switchIndication(com.android.internal.R.string.lockscreen_storage_locked);
             } else if (!TextUtils.isEmpty(mTransientIndication)) {
-                if (powerIndication != null && !mTransientIndication.equals(powerIndication)) {
-                    String indication = mContext.getResources().getString(
-                            R.string.keyguard_indication_trust_unlocked_plugged_in,
-                            mTransientIndication, powerIndication);
-                    mTextView.switchIndication(indication);
-                } else {
-                    mTextView.switchIndication(mTransientIndication);
-                }
+                mTextView.switchIndication(mTransientIndication);
                 isError = mTransientTextIsError;
             } else if (!TextUtils.isEmpty(trustGrantedIndication)
                     && mKeyguardUpdateMonitor.getUserHasTrust(userId)) {

Octavi-OS/platform_frameworks_base

commit e4e2c379f98b322a64085aea2c5f112a1137666a

SystemUI: Fix showing duplicated charging status In commit 85e5a57 it was possible that plugging a device in, then turning on the screen would show the charging status twice. To fix, don't append the charging status to if there is a transient notification.
(Originally 34e1765ecdc5896e7340cf2a3af825864d3dc085, accidentally dropped in r38 merge)

Change-Id: Ic031fbaf9a99d044f50dc65e2b189773222ebe80
Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -531,15 +531,6 @@ public class KeyguardIndicationController implements StateListener,
         if (!mKeyguardUpdateMonitor.isUserUnlocked(userId)) {
             mTextView.switchIndication(com.android.internal.R.string.lockscreen_storage_locked);
         } else if (!TextUtils.isEmpty(mTransientIndication)) {
-            if (powerIndication != null && !mTransientIndication.equals(powerIndication)) {
-                String indication = mContext.getResources().getString(
-                                R.string.keyguard_indication_trust_unlocked_plugged_in,
-                                mTransientIndication, powerIndication);
-                mTextView.switchIndication(indication);
-                hideIndication = !mBatteryPresent;
-            } else {
-                mTextView.switchIndication(mTransientIndication);
-            }
             isError = mTransientTextIsError;
         } else if (!TextUtils.isEmpty(trustGrantedIndication)
                 && mKeyguardUpdateMonitor.getUserHasTrust(userId)) {