f60a8e7d78e4cfebc3a0181208bf1c6cd6ff689e
SagarMakhar <sagarmakhar@gmail.com>
2021-09-09 20:56:05 +0000

    SystemUI: Fix overlays resetting on every reboot Change-Id: I9cd16f3e7c22e5bf50a0fafaf926417f7acb4509 Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -40,6 +40,7 @@ import com.android.systemui.R;
 import com.android.systemui.SystemUI;
 import com.android.systemui.broadcast.BroadcastDispatcher;
 import com.android.systemui.dagger.qualifiers.Background;
+import com.android.systemui.settings.CurrentUserTracker;
 
 import com.google.android.collect.Sets;
 
@@ -72,6 +73,8 @@ public class ThemeOverlayController extends SystemUI {
     private BroadcastDispatcher mBroadcastDispatcher;
     private final Handler mBgHandler;
 
+    private CurrentUserTracker mUserTracker;
+
     @Inject
     public ThemeOverlayController(Context context, BroadcastDispatcher broadcastDispatcher,
             @Background Handler bgHandler) {
@@ -90,7 +93,6 @@ public class ThemeOverlayController extends SystemUI {
                 mContext.getString(R.string.launcher_overlayable_package),
                 mContext.getString(R.string.themepicker_overlayable_package));
         final IntentFilter filter = new IntentFilter();
-        filter.addAction(Intent.ACTION_USER_SWITCHED);
         filter.addAction(Intent.ACTION_MANAGED_PROFILE_ADDED);
         mBroadcastDispatcher.registerReceiverWithHandler(new BroadcastReceiver() {
             @Override
@@ -114,6 +116,15 @@ public class ThemeOverlayController extends SystemUI {
                     }
                 },
                 UserHandle.USER_ALL);
+
+        mUserTracker = new CurrentUserTracker(mBroadcastDispatcher) {
+            @Override
+            public void onUserSwitched(int newUserId) {
+                if (DEBUG) Log.d(TAG, "onUserSwitched");
+                updateThemeOverlays();
+            }
+        };
+        mUserTracker.startTracking();
 
         ContentObserver observer = new ContentObserver(mBgHandler) {
              @Override
