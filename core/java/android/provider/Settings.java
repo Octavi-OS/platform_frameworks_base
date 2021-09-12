d8fa915543a912be4a2c3161bd1184b19bff96f8
nullxception <nullxceptionnullxception@users.noreply.github.com>
2021-09-12 14:35:11 +0000

    SystemUI: Add simple fade filter to the media artwork [1/2] Some artworks may have a bright colors that will makes the title and other elements obscured, lets add some color filter that will fade the album art with already-existing background color for better accessibility.
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6246,6 +6246,13 @@ public final class Settings {
         public static final String ARTWORK_MEDIA_BACKGROUND = "artwork_media_background";
 
         /**
+         * The fade level of Artwork on background media notification
+         * requires ARTWORK_MEDIA_BACKGROUND to be enabled
+         * @hide
+         */
+        public static final String ARTWORK_MEDIA_FADE_LEVEL = "artwork_media_fade_level";
+
+        /**
          * @hide
          */
         public static final String AMBIENT_LIGHT_PULSE_FOR_ALL = "ambient_light_pulse_for_all";
