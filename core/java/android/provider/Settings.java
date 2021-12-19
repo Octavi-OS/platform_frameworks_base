31e68a8181daba55c69c222915c11079d569c3af
IDontCare-05 <IDontCare-05@users.noreply.github.com>
2021-12-19 20:30:24 +0000

    SystemUI: Add seekbar for Left/Right statusbar Paddings [1/2]
    Signed-off-by: Sagarrokade006 <Sagarrokade006@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6337,6 +6337,18 @@ public final class Settings {
         public static final String QS_SHOW_AUTO_BRIGHTNESS_BUTTON = "qs_show_auto_brightness_button";
 
         /**
+         * Whether to add left padding on statusbar
+         * @hide
+         */
+        public static final String LEFT_PADDING = "statusbar_left_padding";
+
+        /**
+         * Whether to add right padding on statusbar
+         * @hide
+         */
+        public static final String RIGHT_PADDING = "statusbar_right_padding";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
